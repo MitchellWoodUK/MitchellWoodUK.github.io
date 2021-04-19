fd3e53be7d5df3453a0916f2cbeda3fdea0787c1
MitchellWoodUK <mitchell_wood@btinternet.com>
2021-04-19 13:47:44 +0100

    Fixed sports API issue where it does not delete.

--- Scripts/sportapiCall.js
+++ Scripts/sportapiCall.js
@@ -1,8 +1,6 @@
 function getAPIData()
 {
     var xhttp = new XMLHttpRequest();
-
-
     var searchValue = document.getElementById('search-text').value;
     console.log(searchValue);
     var url = 'https://www.thesportsdb.com/api/v1/json/1/searchevents.php?e=' + searchValue;
@@ -10,33 +8,34 @@ function getAPIData()
 
     console.log(url);
     xhttp.open('GET', url);
-
     xhttp.send();
-
-
     xhttp.onreadystatechange = function()
     {
         if (this.readyState == 4 && this.status == 200)
         {
             data = JSON.parse(this.responseText);
 
-            console.log(data);
+            console.log(data)
+
             document.getElementById('search-text').value = null;
+
+            document.getElementById('search-items').remove();
+
+            var divNode = document.createElement("DIV");
+
+            divNode.id = 'search-items';
+
+            document.getElementById("results-items").appendChild(divNode);
+
             for (var i in data.event)
             {
-
                     var node = document.createElement("H3");
                     var textnode = document.createTextNode('Sport: ' + data.event[i].strSport + ',' + ' League: ' + data.event[i].strLeague + ',' +' Event: ' + data.event[i].strEvent + ',' + ' Date: ' + data.event[i].dateEvent + ',' + ' Time: ' + data.event[i].strTime + '.');
+
                     node.appendChild(textnode);
-
-
-                    document.getElementById("event").appendChild(node);
-
+                    node.style.color = 'white';
+                    document.getElementById("search-items").appendChild(node);
             }
         }
     }
-
-
 }
-
-
